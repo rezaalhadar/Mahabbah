d4eae43a591678c1dda1167736b28e1957fb5cc6
Rido Haddar <72219905+rezaalhadar@users.noreply.github.com>
2023-08-06 19:22:04 +0700

    update

--- app/src/main/java/com/randfiq/mahabbah/ui/user/AddUserFragment.java
+++ app/src/main/java/com/randfiq/mahabbah/ui/user/AddUserFragment.java
@@ -22,9 +22,25 @@ public class AddUserFragment extends Fragment {
 
     private View view;
 
+    // Object Data Pribadi
+    private TextInputEditText inputEditText_nama;
+    private TextInputEditText inputEditText_namaayah;
+    private TextInputEditText inputEditText_marga;
+    private TextInputEditText inputEditText_status;
+
+    // Object Kontak Pribadi
+    private TextInputEditText inputEditText_nomorhp;
+    private TextInputEditText inputEditText_email;
+    private TextInputEditText inputEditText_alamatsekarang;
+
+    // Object Data Identitas
+    private TextInputEditText inputEditText_nikktp;
+    private TextInputEditText inputEditText_tempatlahir;
     private TextInputEditText inputEditText_tanggallahir;
 
-
+    // Object Data Perbankan
+    private AutoCompleteTextView inputEditText_namabank;
+    private TextInputEditText inputEditText_nomorrekening;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,11 +56,34 @@ public class AddUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_add_user, container, false);
 
+        // Set Object Data Pribadi
+        inputEditText_nama = view.findViewById(R.id.textfield_adduser_nama);
+        inputEditText_namaayah = view.findViewById(R.id.textfield_adduser_ayah);
+        inputEditText_marga = view.findViewById(R.id.textfield_adduser_marga);
+        inputEditText_status = view.findViewById(R.id.textfield_adduser_status);
+
+        // Set Object Kontak Pribadi
+        inputEditText_nomorhp = view.findViewById(R.id.textfield_adduser_nomorhp);
+        inputEditText_email = view.findViewById(R.id.textfield_adduser_email);
+        inputEditText_alamatsekarang = view.findViewById(R.id.textfield_adduser_alamat);
+
+        // Set Object Data Identitas
+        inputEditText_nikktp = view.findViewById(R.id.textfield_adduser_nikktp);
+        inputEditText_tempatlahir = view.findViewById(R.id.textfield_adduser_tempatlahir);
         inputEditText_tanggallahir = view.findViewById(R.id.textfield_adduser_tanggallahir);
 
-     // Get the AutoCompleteTextView reference from XML layout
-        AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.textfield_adduser_namabank);
+        // Set Object Data Perbankan
+        inputEditText_namabank = view.findViewById(R.id.textfield_adduser_bank);
+        inputEditText_nomorrekening = view.findViewById(R.id.textfield_adduser_nomorrekening);
 
+        setupAdapterBankList();
+        inputEditText_tanggallahir.setOnClickListener(v -> showDatePickerDialog());
+
+        return view;
+
+    }
+
+    private void setupAdapterBankList(){
         // Create an ArrayAdapter with the suggestions
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 requireContext(), android.R.layout.simple_dropdown_item_1line,
@@ -52,12 +91,7 @@ public class AddUserFragment extends Fragment {
         );
 
         // Set the adapter for the AutoCompleteTextView
-        autoCompleteTextView.setAdapter(adapter);
-
-        inputEditText_tanggallahir.setOnClickListener(v -> showDatePickerDialog());
-
-        return view;
-
+        inputEditText_namabank.setAdapter(adapter);
     }
 
     public void showDatePickerDialog() {
