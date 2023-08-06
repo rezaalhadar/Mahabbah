a257510a8b778e28f05e4a4922d05668acd27bce
Rido Haddar <72219905+rezaalhadar@users.noreply.github.com>
2023-08-06 22:26:37 +0700

    update

---
+++ app/src/main/java/com/randfiq/mahabbah/utils/Constant.java
@@ -0,0 +1,36 @@
+package com.randfiq.mahabbah.utils;
+
+public interface Constant {
+
+    // TODO WebApp Google Apps Script
+
+    // Sample Url Web App
+    // https://script.google.com/macros/s/AKfycbzzZxxxxxxxxxxxxxxxxxxxxxxrg4KQjS9d0/exec?action=getItems
+    // https://script.google.com/macros/s/AKfycbzqCw8ydb13cVpkVfky5NMlkAJaHASkc7XugvwD2AAZPKLDmhegkZEcgIrzz_NKfmyp/exec
+
+    String WebAppBaseURL = "https://script.google.com/macros/s/AKfycbzqCw8ydb13cVpkVfky5NMlkAJaHASkc7XugvwD2AAZPKLDmhegkZEcgIrzz_NKfmyp/exec";
+    String WebApp_ActionGetDatas = "https://script.google.com/macros/s/AKfycbzqCw8ydb13cVpkVfky5NMlkAJaHASkc7XugvwD2AAZPKLDmhegkZEcgIrzz_NKfmyp/exec?action=getDatas";
+
+    // Data Key
+    String key_keyRootData = "items";
+
+    // Data Key - Data Diri
+    String key_nama = "nama";
+    String key_namaayah = "nama_ayah";
+    String key_marga = "marga";
+    String key_status = "status";
+
+    // Data Key - Kontak
+    String key_nomor_hp = "nomor_hp";
+    String key_email = "email";
+    String key_alamat = "alamat";
+
+    // Data Key - Data Identitas
+    String key_nik_ktp = "nik_ktp";
+    String key_tempat_lahir = "tempat_lahir";
+    String key_tanggal_lahir = "tanggal_lahir";
+
+    // Data Key - Data Perbankan
+    String key_nama_bank = "nama_bank";
+    String key_nomor_rekening = "nomor_rekening";
+}
